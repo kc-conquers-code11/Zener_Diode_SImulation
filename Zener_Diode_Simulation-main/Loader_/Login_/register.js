a0afc4fa6807219f284a8b37df49529033ae260b
Krishna Choudhary <104185486+kc-conquers-code11@users.noreply.github.com>
2025-11-21 00:42:45 +0530

    Remove Firebase config from register.js
    Removed sensitive Firebase configuration details from register.js.

--- Zener_Diode_Simulation-main/Loader_/Login_/register.js
+++ Zener_Diode_Simulation-main/Loader_/Login_/register.js
@@ -5,14 +5,7 @@ import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11
 
 
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDYPE1JvPCUa6LDLekmXosmFQ-SegUAe0Y",
-    authDomain: "zener-login.firebaseapp.com",
-    projectId: "zener-login",
-    storageBucket: "zener-login.appspot.com",
-    messagingSenderId: "1066231166290",
-    appId: "1:1066231166290:web:546eb712962a18818c368b"
-};
+//hidden
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
